3991b402e49f64935870cc0a02b8309501de244a
slowrunner <slowrunner@users.noreply.github.com>
2024-09-13 18:32:43 -0400

    rm log/ install/

--- ros2ws/install/dave_interfaces/share/dave_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(dave_interfaces_IDL_FILES "srv/Say.idl;srv/Dock.idl;srv/Undock.idl;msg/BatteryState.idl;msg/DockStatus.idl")
-set(dave_interfaces_INTERFACE_FILES "srv/Say.srv;srv/Say_Request.msg;srv/Say_Response.msg;srv/Dock.srv;srv/Dock_Request.msg;srv/Dock_Response.msg;srv/Undock.srv;srv/Undock_Request.msg;srv/Undock_Response.msg;msg/BatteryState.msg;msg/DockStatus.msg")
--- ros2ws/install/ros2_gopigo3_msg/share/ros2_gopigo3_msg/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(ros2_gopigo3_msg_IDL_FILES "msg/MotorStatus.idl;msg/MotorStatusLR.idl;srv/SPI.idl")
-set(ros2_gopigo3_msg_INTERFACE_FILES "msg/MotorStatus.msg;msg/MotorStatusLR.msg;srv/SPI.srv;srv/SPI_Request.msg;srv/SPI_Response.msg")
